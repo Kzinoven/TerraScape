1bcb35adaa975b570082f5504b2f97f495ec0f15
benlerner <benlerner@bitbucket.com>
2015-01-17 18:27:08 -0500

    Created basic AI for shield creature
    Also updated testing GUI to edit creature parameters

---
+++ Unity_Files/Assets/Scripts/BasicEnemyAI.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyAI : MonoBehaviour {
+	static GameObject Player;
+
+	public float attackSpeed = 1.0f;
+	float attackCooldown;
+	float attackTimer = 0;
+	public float attackDamage = 5.0f;
+
+	public float wanderSpeed = 2.0f;
+	public float chaseSpeed = 5.0f;
+	public float chaseWaitTime = 5.0f; //amount of time to wait when last known position is reached.
+	float chaseTimer;
+
+	public float rotationSpeed;
+	private Quaternion _lookRotation;
+	private Vector3 _direction;
+
+	public float fieldOfView = 200f;
+	public float viewRange = 20f;
+	public float attackRange = 4f;
+	
+	Vector3 lastPlayerSighting;
+
+	Rigidbody collider;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindWithTag ("Player");
+		attackCooldown = 1.0f / attackSpeed;
+		chaseTimer = chaseWaitTime;
+		collider = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		attackTimer += Time.deltaTime;
+		if (playerVisible()) {
+			//if the player is within attack range
+			if (DistanceFromPlayer () <= attackRange) {
+				Attack ();
+			}
+			lastPlayerSighting = Player.transform.position;
+			chaseTimer = 0;
+		}
+		if (chaseTimer < chaseWaitTime) {
+			//go to last known location
+			Chase();
+		}
+		else {
+			Wander();
+		}
+	}
+
+	float DistanceFromPlayer(){
+		return Vector3.Distance (Player.transform.position, transform.position);
+	}
+
+	//check if player is visible using enemy FOV and viewRange, possibly use raycasting/hiding behind obstacles in a future update
+	bool playerVisible(){
+		return DistanceFromPlayer() <= viewRange;
+	}
+
+	//use attackTimer to regulate attack speed
+	void Attack(){
+		if (attackTimer >= attackCooldown) {
+			Player.SendMessage("TakeDamage", attackDamage);
+			attackTimer = 0f;
+		}
+	}
+
+	void Wander(){
+
+	}
+
+	//move towards player's last known position
+	void Chase(){
+		//we don't want the enemy to move directly in the y direction, so set last known positions y to this object's y
+		Vector3 destination = new Vector3 (lastPlayerSighting.x, transform.position.y, lastPlayerSighting.z);
+
+		_direction = (destination - transform.position).normalized;
+		_lookRotation = Quaternion.LookRotation(_direction);
+
+		//move and turn towards last known player position.
+		collider.MoveRotation (_lookRotation);
+
+		// If near the last  sighting...
+		if(Vector3.Distance(transform.position, destination) < attackRange - 0.5)
+		{
+			// ... increment the timer, waiting at that spot until player is detected or timer runs out.
+			chaseTimer += Time.deltaTime;
+		}
+		else {
+			//only move if not near the player/sighting
+			collider.MovePosition (transform.position + _direction * chaseSpeed * Time.deltaTime);
+		}
+	}
+}
--- Unity_Files/Assets/Scripts/PlaytesterMenu.cs
+++ Unity_Files/Assets/Scripts/PlaytesterMenu.cs
@@ -9,6 +9,8 @@ public class PlaytesterMenu : MonoBehaviour {
 	GameObject player;
 	TP_Motor playerMotor;
 
+	BasicEnemyAI enemyTest;
+
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag ("Player");
@@ -19,6 +21,8 @@ public class PlaytesterMenu : MonoBehaviour {
 		if (playerMotor == null) {
 			throw new UnityException("Player has no TP_Motor component.");
 		}
+
+		enemyTest = GameObject.Find ("shieldCreature_inProgress_v5_unsmoothed_walk").GetComponent<BasicEnemyAI> ();
 
 		//make the overlay the same size as the screen
 		textureOverlay.transform.position = new Vector3(0, 0, overlayDepth);
@@ -46,22 +50,46 @@ public class PlaytesterMenu : MonoBehaviour {
 		}
 	}
 
+	#region Editor Variables
 	string walkSpeed = "8";
 	string jumpSpeed = "6";
+	string enemyAtkSpeed = "1";
+	string enemyAtkDamage = "5";
+	string enemyAtkRange = "4";
+	string enemyChaseSpeed = "5";
+	string enemySightRange = "20";
+	float labelWidth = 150;
 
+	float newWalkSpeed;
+	float newJumpSpeed;
+	float newAtkSpeed;
+	float newAtkDamage;
+	float newAtkRange;
+	float newChaseSpeed;
+	float newSightRange;
+	#endregion
 	//create GUI elements in here
 	void OnGUI (){
 		if (!enabled) 
 		{return;}
 
-		walkSpeed = labelTxtField (new Rect (20, 20, 100, 20), walkSpeed, "Walk Speed");
+		walkSpeed = labelTxtField (new Rect (20, 20, labelWidth, 20), walkSpeed, "Walk Speed");
 
-		jumpSpeed = labelTxtField (new Rect (20, 50, 100, 20), jumpSpeed, "Jump Speed");
+		jumpSpeed = labelTxtField (new Rect (20, 50, labelWidth, 20), jumpSpeed, "Jump Speed");
+
+		enemyAtkSpeed = labelTxtField (new Rect (20, 80, labelWidth, 20), enemyAtkSpeed, "Enemy Attack Speed");
+
+		enemyAtkDamage = labelTxtField (new Rect (20, 110, labelWidth, 20), enemyAtkDamage, "Enemy Attack Damage");
+
+		enemyAtkRange = labelTxtField (new Rect (20, 140, labelWidth, 20), enemyAtkRange, "Enemy Attack Range");
+
+		enemyChaseSpeed = labelTxtField (new Rect (20, 170, labelWidth, 20), enemyChaseSpeed, "Enemy Walk Speed");
+
+		enemySightRange = labelTxtField (new Rect (20, 200, labelWidth, 20), enemySightRange, "Enemy Sight Range");
 		
 		//if the GUI has been changed, validate each value and update parameters accordingly 
 		if (GUI.changed) {
-			float newWalkSpeed;
-			float newJumpSpeed;
+
 			if (float.TryParse(walkSpeed, out newWalkSpeed)){
 				playerMotor.forwardSpeed = newWalkSpeed;
 				playerMotor.backwardSpeed = newWalkSpeed;
@@ -72,6 +100,25 @@ public class PlaytesterMenu : MonoBehaviour {
 				playerMotor.jumpSpeed = newJumpSpeed;
 			}
 
+			if (float.TryParse(enemyAtkSpeed, out newAtkSpeed)){
+				enemyTest.attackSpeed = newAtkSpeed;
+			}
+
+			if (float.TryParse(enemyAtkDamage, out newAtkDamage)){
+				enemyTest.attackDamage = newAtkDamage;
+			}
+
+			if (float.TryParse(enemyAtkRange, out newAtkRange)){
+				enemyTest.attackRange = newAtkRange;
+			}
+
+			if (float.TryParse(enemyChaseSpeed, out newChaseSpeed)){
+				enemyTest.chaseSpeed = newChaseSpeed;
+			}
+
+			if (float.TryParse(enemySightRange, out newSightRange)){
+				enemyTest.viewRange = newSightRange;
+			}
 		}
 	}
 
