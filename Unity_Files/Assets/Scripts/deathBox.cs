c4bbb886f7a63187d4f8dd0fea06b004ab852033
alexfatemi <videogamesteralex@hotmail.com>
2015-02-19 22:30:30 -0500

    Added Level Transitions, scenes, and level 2 winable

---
+++ Unity_Files/Assets/Scripts/deathBox.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathBox : MonoBehaviour {
+
+	private GameObject player;
+	private Collider target;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		target = player.GetComponent<CharacterController>();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other != target) //The colliding object isn't our object
+		{
+			//Debug.Log("something's wrong");
+			return; //don't do anything if it's not our target
+		}
+		else {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
